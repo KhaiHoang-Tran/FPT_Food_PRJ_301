caa5e293ed501462976c220cc17ec71ae3c64eae
AnVu05 <vudinhan2k5@gmail.com>
2026-02-11 21:08:28 +0700

    Recipe_all

--- FPT_Food_PRJ/src/java/controller/recipesController.java
+++ FPT_Food_PRJ/src/java/controller/recipesController.java
@@ -2,10 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.FoodDAO;
+import dal.IngredientDAO;
+import dal.RecipeDAO;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -14,39 +15,52 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Food;
+import model.Ingredient;
+import model.Recipe;
 
 /**
  *
  * @author AN
  */
 public class recipesController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         String action = request.getParameter("action");
-        action = action == null ? "": request.getParameter("action");
+        action = action == null ? "" : request.getParameter("action");
         switch (action) {
             case "update":
                 updateStatusFood(request, response);
+                displayRecipesDetail(request, response);
                 break;
-            default:
-                displayMenu(request,response);
+            case "view":
+                displayRecipesDetail(request, response);
+                break;
+            case "add":
+                addRecipes(request, response);
+                break;
+                
         }
-    } 
+        displayMenu(request, response);
+        request.getRequestDispatcher("kitchen.jsp").forward(request, response);
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,12 +68,13 @@ public class recipesController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -67,56 +82,92 @@ public class recipesController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-    private void displayMenu(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException, ServletException, IOException{
+
+    private void displayMenu(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException, ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         String activeSection = "recipes";
         String mesg = "";
-        FoodDAO  fDAO = new FoodDAO();
+        FoodDAO fDAO = new FoodDAO();
         ArrayList<Food> listFood = new ArrayList<>();
         listFood.addAll(fDAO.getAll());
-        if(listFood == null){
+        IngredientDAO iDAO = new IngredientDAO();
+        ArrayList<Ingredient> listIngredient = new ArrayList<>();
+        listIngredient.addAll(iDAO.getAll());
+        if (listFood == null) {
             mesg += "Không có món nào trong menu";
             request.setAttribute("activeSection", activeSection);
             request.setAttribute("mesg", mesg);
-            request.getRequestDispatcher("kitchen.jsp").forward(request, response);
-        }else{
+        } else {
             request.setAttribute("listFood", listFood);
             request.setAttribute("activeSection", activeSection);
-            request.getRequestDispatcher("kitchen.jsp").forward(request, response);
+            request.setAttribute("listIngredient", listIngredient);
         }
     }
 
     private void updateStatusFood(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException, ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html;charset=UTF-8");       
-        int foodID = Integer.parseInt(request.getParameter("foodId"));
-        int selectedFoodId = foodID;
-        String status = request.getParameter("status");
+        response.setContentType("text/html;charset=UTF-8");
         FoodDAO fDAO = new FoodDAO();
-        int check = fDAO.updateStatus(foodID,status);
-        if(check >=0){
+        int foodID = Integer.parseInt(request.getParameter("foodId"));
+        String status = request.getParameter("status");
+        int check = fDAO.updateStatus(foodID, status);
+        if (check >= 0) {
             System.out.println("So dong da duoc cap nhat la " + check);
-            request.setAttribute("selectedFoodId", selectedFoodId);
-            displayMenu(request, response);
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    private void displayRecipesDetail(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        FoodDAO fDAO = new FoodDAO();
+        int foodID = Integer.parseInt(request.getParameter("foodId"));
+        RecipeDAO rDAO = new RecipeDAO();
+        IngredientDAO iDAO = new IngredientDAO();
+        Food selectedFood = fDAO.getById(foodID);
+        ArrayList<Recipe> listRecipe = rDAO.getAllByFoodID(foodID);
+        ArrayList<Ingredient> listIngredient = iDAO.getAll();
+        request.setAttribute("selectedFood", selectedFood);
+        request.setAttribute("listRecipe", listRecipe);
+        request.setAttribute("listIngredient", listIngredient);
+    }
+
+    private void addRecipes(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        int foodID = Integer.parseInt(request.getParameter("foodId"));
+        int ingId = Integer.parseInt(request.getParameter("ingId"));
+        Double amountNeeded = Double.parseDouble(request.getParameter("amountNeeded"));
+        Recipe r = Recipe.builder()
+                .foodID(foodID)
+                .ingredientID(ingId)
+                .amountNeeded(amountNeeded).build();
+        RecipeDAO rDAO = new RecipeDAO();
+        int check = rDAO.InsertNewRecipe(r);
+        if(check > 0){
+            System.out.println("So dong da thay doi la: "+ check);
         }else{
             System.out.println("Error");
-            request.setAttribute("selectedFoodId", selectedFoodId);
-            displayMenu(request, response);
         }
+        displayRecipesDetail(request, response);
     }
 }
--- FPT_Food_PRJ/src/java/dal/RecipeDAO.java
+++ FPT_Food_PRJ/src/java/dal/RecipeDAO.java
@@ -4,10 +4,64 @@
  */
 package dal;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Recipe;
+
 /**
  *
  * @author AN
  */
 public class RecipeDAO extends DBContext {
-    
+
+    public ArrayList<Recipe> getAllByFoodID(int foodID) {
+        ArrayList<Recipe> listRecipe = new ArrayList<>();
+        connection = getConnection();
+        String sql = "SELECT *\n"
+                + "  FROM [dbo].[Recipe]\n"
+                + "  Where foodID = ?";
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, foodID);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int recipeID = resultSet.getInt("recipeID");
+                int ingredientID = resultSet.getInt("ingredientID");
+                Double amountNeeded = resultSet.getDouble("amountNeeded");
+                Recipe r = Recipe.builder()
+                        .recipeID(recipeID)
+                        .ingredientID(ingredientID)
+                        .foodID(foodID)
+                        .amountNeeded(amountNeeded).build();
+                listRecipe.add(r);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        return listRecipe;
+    }
+
+    public int InsertNewRecipe(Recipe r) {
+        int resultSet = 0;
+        connection = getConnection();
+        String sql = "INSERT INTO [dbo].[Recipe]\n"
+                + "           ([foodID]\n"
+                + "           ,[ingredientID]\n"
+                + "           ,[amountNeeded])\n"
+                + "     VALUES\n"
+                + "           (?,?,?)";
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, r.getFoodID());
+            statement.setInt(2, r.getIngredientID());
+            statement.setDouble(3, r.getAmountNeeded());
+            resultSet = statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        return resultSet;
+    }
+
 }
